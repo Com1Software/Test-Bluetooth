b3d96e28ad66fbda0f90f1b133b00a92e42da035
Lenovo 15 <information@com1software.com>
2022-10-17 13:22:23 -0400

    cleanup

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"tinygo.org/x/bluetooth"
 )
 
@@ -8,29 +10,28 @@ var adapter = bluetooth.DefaultAdapter
 
 func main() {
 	must("enable BLE stack", adapter.Enable())
-	test:=1
-	
-	switch{
-	
-		case test ==1:
-		adv := adapter.DefaultAdvertisement()
-	         must("config adv", adv.Configure(bluetooth.AdvertisementOptions{
-		         LocalName: "Go Bluetooth",
-	                 }))
-	must("start adv", adv.Start())
+	test := 1
+	switch {
 
-	println("advertising...")
-	address, _ := adapter.Address()
-	for {
-		println("Go Bluetooth /", address.MAC.String())
-		time.Sleep(time.Second)
-	}
-		case test==0:
-	println("scanning...")
-	err := adapter.Scan(func(adapter *bluetooth.Adapter, device bluetooth.ScanResult) {
-		println("found device:", device.Address.String(), device.RSSI, device.LocalName())
-	})
-	must("start scan", err)
+	case test == 1:
+		adv := adapter.DefaultAdvertisement()
+		must("config adv", adv.Configure(bluetooth.AdvertisementOptions{
+			LocalName: "Go Bluetooth",
+		}))
+		must("start adv", adv.Start())
+
+		println("advertising...")
+		address, _ := adapter.Address()
+		for {
+			println("Go Bluetooth /", address.MAC.String())
+			time.Sleep(time.Second)
+		}
+	case test == 0:
+		println("scanning...")
+		err := adapter.Scan(func(adapter *bluetooth.Adapter, device bluetooth.ScanResult) {
+			println("found device:", device.Address.String(), device.RSSI, device.LocalName())
+		})
+		must("start scan", err)
 	}
 }
 
