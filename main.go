b527f4477ca72a5bd1263a58f621082baa8c0683
Com1 Software, Inc <information@com1software.com>
2022-10-17 13:20:30 -0400

    Update main.go

--- main.go
+++ main.go
@@ -7,15 +7,31 @@ import (
 var adapter = bluetooth.DefaultAdapter
 
 func main() {
-	// Enable BLE interface.
 	must("enable BLE stack", adapter.Enable())
+	test:=1
+	
+	switch{
+	
+		case test ==1:
+		adv := adapter.DefaultAdvertisement()
+	         must("config adv", adv.Configure(bluetooth.AdvertisementOptions{
+		         LocalName: "Go Bluetooth",
+	                 }))
+	must("start adv", adv.Start())
 
-	// Start scanning.
+	println("advertising...")
+	address, _ := adapter.Address()
+	for {
+		println("Go Bluetooth /", address.MAC.String())
+		time.Sleep(time.Second)
+	}
+		case test==0:
 	println("scanning...")
 	err := adapter.Scan(func(adapter *bluetooth.Adapter, device bluetooth.ScanResult) {
 		println("found device:", device.Address.String(), device.RSSI, device.LocalName())
 	})
 	must("start scan", err)
+	}
 }
 
 func must(action string, err error) {
